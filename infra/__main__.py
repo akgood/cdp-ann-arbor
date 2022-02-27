dc20673c457150d463c6f0242e5927f77b851eb0
Adam Goodman <akgood@adams-air.lan>
2022-02-27 09:11:21 -0500

    Initial commit

---
+++ infra/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cdp_backend.infrastructure import CDPStack
+from pulumi import export
+
+###############################################################################
+
+cdp_stack = CDPStack(
+    gcp_project_id="cdp-ann-arbor-mytqnzzk",
+    municipality_name="Ann Arbor",
+    firestore_location="us-central",
+    hosting_github_url="https://github.com/akgood/cdp-ann-arbor",
+    hosting_web_app_address="https://akgood.github.io/cdp-ann-arbor",
+    governing_body="city council",
+)
+
+export("firestore_address", cdp_stack.firestore_app.app_id)
+export("gcp_bucket_name", cdp_stack.firestore_app.default_bucket)
