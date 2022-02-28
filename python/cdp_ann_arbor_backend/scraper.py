65da16347cc76cded5a5439e86098ffd795874e2
Adam Goodman <akgood@adams-air.lan>
2022-02-27 19:54:01 -0500

    fix scraper

--- python/cdp_ann_arbor_backend/scraper.py
+++ python/cdp_ann_arbor_backend/scraper.py
@@ -140,3 +140,33 @@ class AnnArborScraper(LegistarScraper):
             return []
 
         return [ContentURIs(vod_url, vod_caption_url)]
+
+def get_events(
+    from_dt: datetime,
+    to_dt: datetime,
+    **kwargs,
+) -> List[EventIngestionModel]:
+    """
+    Get all events for the provided timespan.
+
+    Parameters
+    ----------
+    from_dt: datetime
+        Datetime to start event gather from.
+    to_dt: datetime
+        Datetime to end event gather at.
+
+    Returns
+    -------
+    events: List[EventIngestionModel]
+        All events gathered that occured in the provided time range.
+
+    Notes
+    -----
+    As the implimenter of the get_events function, you can choose to ignore the from_dt
+    and to_dt parameters. However, they are useful for manually kicking off pipelines
+    from GitHub Actions UI.
+    """
+
+    # Your implementation here
+    return AnnArborScraper().get_events(from_dt, to_dt, **kwargs)
