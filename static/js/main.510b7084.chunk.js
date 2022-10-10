ba295b456732243f7a15a749be784565a744615c
akgood <akgood@users.noreply.github.com>
2022-10-10 23:49:06 +0000

    Deploying to gh-pages from main @ c86c61bafa405d4612911d86e87275245f8d5d67 🚀

---
+++ static/js/main.510b7084.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcdp-ann-arbor"]=this["webpackJsonpcdp-ann-arbor"]||[]).push([[0],{203:function(n,e){},90:function(n,e,o){"use strict";o.r(e);o(0);var r=o(26),t=o.n(r),i=o(65),c=(o(217),o(57));t.a.render(Object(c.jsx)("div",{children:Object(c.jsx)(i.AppConfigProvider,{appConfig:{firebaseConfig:{options:{projectId:"cdp-ann-arbor-mytqnzzk"},settings:{}},municipality:{name:"Ann Arbor",footerLinksSections:[]}},children:Object(c.jsx)(i.App,{})})}),document.getElementById("root"))}},[[90,1,2]]]);
+//# sourceMappingURL=main.510b7084.chunk.js.map
